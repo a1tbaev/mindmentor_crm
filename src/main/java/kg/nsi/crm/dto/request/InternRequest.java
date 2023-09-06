8114648da60cbe1e6c73c1e1c63c623333b5c5dc
kkutman <kkutman18@gmail.com>
2023-09-06 12:08:04 +0600

    add phone number and password valid

--- src/main/java/kg/nsi/crm/dto/request/InternRequest.java
+++ src/main/java/kg/nsi/crm/dto/request/InternRequest.java
@@ -1,14 +1,15 @@
 package kg.nsi.crm.dto.request;
 
 import kg.nsi.crm.enums.InternStatus;
+import kg.nsi.crm.validation.PhoneNumberValid;
 import lombok.Builder;
 
 @Builder
 public record InternRequest (
-
         String name,
         String surname,
         String email,
+        @PhoneNumberValid
         String phoneNumber,
         Long stackId,
         boolean isPaid,
