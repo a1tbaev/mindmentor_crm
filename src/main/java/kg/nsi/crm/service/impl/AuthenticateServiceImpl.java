b7c366f52759265f95a712d3d346776e52527356
kkutman <kkutman18@gmail.com>
2023-09-07 11:49:32 +0600

    add exception handling

--- src/main/java/kg/nsi/crm/service/impl/AuthenticateServiceImpl.java
+++ src/main/java/kg/nsi/crm/service/impl/AuthenticateServiceImpl.java
@@ -5,6 +5,7 @@ import kg.nsi.crm.config.jwt.JwtService;
 import kg.nsi.crm.dto.request.AuthenticateRequest;
 import kg.nsi.crm.dto.response.AuthenticateResponse;
 import kg.nsi.crm.entity.User;
+import kg.nsi.crm.exception.exceptions.BadCredentialException;
 import kg.nsi.crm.repository.UserRepository;
 import kg.nsi.crm.service.AuthenticateService;
 import lombok.RequiredArgsConstructor;
@@ -30,13 +31,9 @@ public class AuthenticateServiceImpl implements AuthenticateService {
 
         User userInfo = userRepository.findByUsername(request.userName())
                 .orElseThrow(() -> {
-                    log.error(String.format("Пользователь с адресом электронной почты %s не существует", request.userName()));
-                    try {
-                        throw new ChangeSetPersister.NotFoundException();
-                    } catch (ChangeSetPersister.NotFoundException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+            log.error(String.format("Пользователь с адресом электронной почты %s не существует", request.userName()));
+                    return new BadCredentialException(String.format("Пользователь с адресом электронной почты %s не существует", request.userName()));
+        });
         if (!passwordEncoder.matches(request.password(), userInfo.getPassword())) {
             log.error("Пароль не подходит");
         }
