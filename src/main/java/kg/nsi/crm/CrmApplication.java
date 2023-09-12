83842209985315f102a047bc05d6ec8816ed8f75
Aidana <akimovadna@gmail.com>
2023-09-12 17:00:19 +0600

    comm

--- src/main/java/kg/nsi/crm/CrmApplication.java
+++ src/main/java/kg/nsi/crm/CrmApplication.java
@@ -1,17 +1,6 @@
 package kg.nsi.crm;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
-
 
 @SpringBootApplication
 public class CrmApplication {
