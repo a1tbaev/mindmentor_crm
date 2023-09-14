c5296898ac0b962fa7b5ce80262cc2117be3a7cc
kkutman <kkutman18@gmail.com>
2023-09-14 14:24:36 +0600

    commit-2

--- src/main/java/kg/nsi/crm/config/security/OpenApiConfig.java
+++ src/main/java/kg/nsi/crm/config/security/OpenApiConfig.java
@@ -17,13 +17,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 title = "CRM system API",
                 version = "1.0"
                  ),
-                servers =
-                {
-                    @Server(
-                            description = "Local DEV",
-                            url = "https://mindmentor-back-4e278328d2f5.herokuapp.com/swagger-ui/index.html#/Vacancy/createVacancy"
-                    )
-                })
+        servers = {@Server(url="https://mindmentor-back-4e278328d2f5.herokuapp.com")})
 @SecurityScheme(
         name = "bearerAuth",
         description = "JWT Auth description",
