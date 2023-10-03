54cd0f0873e9152bd3ac9fb331b8704776bac750
kkutman <kkutman18@gmail.com>
2023-10-03 17:43:29 +0600

    sd

--- src/main/java/kg/nsi/crm/config/security/SecurityConfig.java
+++ src/main/java/kg/nsi/crm/config/security/SecurityConfig.java
@@ -53,7 +53,6 @@ public class SecurityConfig {
                 .and()
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
         return http.build();
     }
 }
