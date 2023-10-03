4b6c6327bb7c6e47b9066e470495208b64a1e39e
kkutman <kkutman18@gmail.com>
2023-10-03 17:33:31 +0600

    dffd

--- src/main/java/kg/nsi/crm/config/security/SecurityConfig.java
+++ src/main/java/kg/nsi/crm/config/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf()
+        http.csrf().and().cors()
                 .disable()
                 .authorizeHttpRequests()
                 .requestMatchers(
