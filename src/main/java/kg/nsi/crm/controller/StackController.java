dc9e9f1bebe17e35b4a66ca6015181ae64e0981b
Aidana <akimovadna@gmail.com>
2023-10-09 16:35:44 +0600

    stack ref

--- src/main/java/kg/nsi/crm/controller/StackController.java
+++ src/main/java/kg/nsi/crm/controller/StackController.java
@@ -3,16 +3,16 @@ package kg.nsi.crm.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.nsi.crm.dto.response.SimpleResponse;
+import kg.nsi.crm.dto.response.StackResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import kg.nsi.crm.entity.Stack;
 import kg.nsi.crm.service.impl.StackServiceImpl;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/stack")
@@ -20,12 +20,17 @@ import lombok.RequiredArgsConstructor;
 @Tag(name = "Stack", description = "The Stack API")
 public class StackController {
 	
-	private final StackServiceImpl serviceImpl;
+	private final StackServiceImpl stackService;
 
 	@Operation(summary = "Create a new stack", description = "This method to create a new stack")
 	@PostMapping("/")
 	public SimpleResponse createStack(@RequestBody Stack stack){
-		return serviceImpl.createStack(stack);
+		return stackService.createStack(stack);
 
+	}
+	@Operation(summary = "Get all the stacks", description = "This method is to get all stacks")
+	@GetMapping("/")
+	public List<StackResponse> getAll(){
+		return stackService.getAll();
 	}
 }
