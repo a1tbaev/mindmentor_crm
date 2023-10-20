e00e19bf52c2ae10e6625cf49fb1222b0c82ec21
Aidana <akimovadna@gmail.com>
2023-10-20 11:00:56 +0600

    vendors id added ref

--- src/main/java/kg/nsi/crm/dto/response/VendorResponse.java
+++ src/main/java/kg/nsi/crm/dto/response/VendorResponse.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 
 @Builder
 public record VendorResponse (
+        Long id,
         String name,
         String img,
         String email,
--- src/main/java/kg/nsi/crm/repository/custom/VendorCustom.java
+++ src/main/java/kg/nsi/crm/repository/custom/VendorCustom.java
@@ -3,7 +3,8 @@ package kg.nsi.crm.repository.custom;
 public class VendorCustom {
     public String getAllVendorQuery(){
         return """
-                SELECT v.name           AS name,
+                SELECT v.id             AS id,
+                        v.name           AS name,
                        v.image          AS img,
                        v.address        AS address,
                        v.contact_number AS phone_number,
